d4f36b15720348b7e85d888c96d9eeffa6ea20dc
kkavitah83-dev <kkavitah83@gmail.com>
2026-04-15 15:59:27 +0530

    Update App.py

--- App.py
+++ App.py
@@ -4,7 +4,7 @@ from transformers import pipeline
 #Load the summarization model
 @st.cache_resource
 def load_summarizer():
-  return pipeline("summarization",model="")
+  return pipeline("summarization",model="ARTeLab/mbart-summarization-fanpage")
 
 summarizer=load_summarizer()
 st.title("AI Text Summarize")
